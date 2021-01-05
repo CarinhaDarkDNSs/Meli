33e8a248a62b6e8afa638c4f5a6449c76c404cf0
CarinhaDarkDNSs <zenixmc.suporte@gmail.com>
2021-01-05 14:29:47 -0300

    First cmmit

---
+++ src/help.js
@@ -0,0 +1,22 @@
+const help = (prefix) => {
+	return `🤖 * - BOT* 🤖\n
+🖥️ *Comandos:* 🖥️\n\n
+📍 comando: *${prefix}info*
+desc : veja as informações da host.\n\n
+📍 comando: *${prefix}imagem*
+desc : pegue a imagem de uma figurinha.\n\n
+📍 comando: *${prefix}marcar*
+desc : marque todos membros do grupo.\n\n
+📍 comandi:  *${prefix}clonar*
+desc: copie a foto de um membro.\n\n
+📍 comando:  *${prefix}sticker*
+desc: crie figurinha com uma foto/video/gif.\n\n
+📍 comando:  *${prefix}banir*
+desc: banir um membro do grupo.\n\n
+📍 comando:  *${prefix}add*
+desc: meliodas - Bot*`
+}
+
+exports.help = help
+
+
